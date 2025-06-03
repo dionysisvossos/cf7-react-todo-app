92290471166ebdd96c0e0cb717eabcef6d5444de
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-06-03 19:41:31 +0300

    feat: add clear all tasks and stats count task

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,12 @@
-import {useReducer} from 'react';
+import {useEffect, useReducer} from 'react';
 import TodoForm from "./TodoForm.tsx";
 import TodoList from "./TodoList.tsx"
 import type {TodoProps, Action} from "./types.ts";
+
+const getInitialTodos = () => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [];
+};
 
 const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
     switch (action.type) {
@@ -19,7 +24,7 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
                 todo.id === action.payload.id
                     ? {...todo, text: action.payload.newText}
                     : todo
-            );
+            )
         }
 
         case "COMPLETE": {
@@ -27,15 +32,30 @@ const todoReducer = (state: TodoProps[], action: Action): TodoProps[] => {
                 todo.id === action.payload
                     ? {...todo, completed: !todo.completed}
                     : todo
-            );
+            )
         }
+
+        case "CLEAR_ALL":
+            return [];
+
         default:
             return state;
     }
 };
 
 const Todo = () =>{
-    const [todos, dispatch] = useReducer(todoReducer, []);
+    const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos);
+    const totalTasks: number = todos.length;
+    const completedTasks: number = todos.filter(todo => todo.completed).length;
+    const pendingTasks: number = totalTasks - completedTasks;
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const handleClearAll = () => {
+        dispatch({type: "CLEAR_ALL"});
+    };
 
     return (
         <>
@@ -43,6 +63,24 @@ const Todo = () =>{
                 <h1 className="text-center text-2xl mb-4">To-Do List</h1>
                 <TodoForm dispatch={dispatch} />
                 <TodoList todos={todos} dispatch={dispatch} />
+
+                {todos.length > 0 && (
+                    <>
+                        <div className="flex justify-between border-t pt-4 mt-4 text-cf-gray">
+                            <span>Total: {totalTasks}</span>
+                            <span>Pending: {pendingTasks}</span>
+                            <span>Completed: {completedTasks}</span>
+                        </div>
+                        <div className="text-end mt-4">
+                            <button
+                                onClick={handleClearAll}
+                                className="bg-cf-dark-red text-white py-2 px-4 rounded ">
+                                Clear All
+                            </button>
+                        </div>
+                    </>
+                )}
+
             </div>
         </>
     )
